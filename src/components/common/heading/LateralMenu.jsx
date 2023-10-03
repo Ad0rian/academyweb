86e1ceab5cbf10c544a569da954ad8f824759df4
Ad0rian <justisatroubleday@gmail.com>
2023-10-03 18:45:02 +0200

    a
    a

--- src/components/common/heading/LateralMenu.jsx
+++ src/components/common/heading/LateralMenu.jsx
@@ -3,7 +3,7 @@ import "./header.css";
 import Box from '@mui/material/Box';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { TreeView } from '@mui/x-tree-view/TreeView';
+import { TreeView } from '@mui/x-tree-view/TreeView'; 
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
 /*
